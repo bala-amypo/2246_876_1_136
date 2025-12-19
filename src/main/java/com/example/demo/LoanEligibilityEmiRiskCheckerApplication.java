8760aa8bb7fd021141fe63f4f32a995d93e1c264
bala-amypo <bala@amypo.in>
2025-12-19 18:17:00 +0000

    User commit

--- src/main/java/com/example/demo/LoanEligibilityEmiRiskCheckerApplication.java
+++ src/main/java/com/example/demo/LoanEligibilityEmiRiskCheckerApplication.java
@@ -1,13 +1,15 @@
+
+
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+@ServletComponentScan // Required for @WebServlet to work
+public class LoanEligibilityEmiRiskCheckerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoanEligibilityEmiRiskCheckerApplication.class, args);
+    }
 }
