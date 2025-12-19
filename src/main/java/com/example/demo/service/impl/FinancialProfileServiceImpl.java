9b5712feab8f8fc01007ec9fdbdae640127a0e87
bala-amypo <bala@amypo.in>
2025-12-19 05:46:10 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/FinancialProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FinancialProfileServiceImpl.java
@@ -1,1 +1,20 @@
-kniininmimimm
+package com.example.demo.service;
+
+import com.example.demo.entity.FinancialProfile;
+import com.example.demo.repository.FinancialProfileRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FinancialProfileServiceImpl implements FinancialProfileService {
+
+    private final FinancialProfileRepository repository;
+
+    public FinancialProfileServiceImpl(FinancialProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public FinancialProfile saveProfile(FinancialProfile profile) {
+        return repository.save(profile);
+    }
+}
--- src/main/java/com/example/demo/service/impl/LoanRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoanRequestServiceImpl.java
@@ -1,1 +1,20 @@
-ghgyhujij
+package com.example.demo.service;
+
+import com.example.demo.entity.LoanRequest;
+import com.example.demo.repository.LoanRequestRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanRequestServiceImpl implements LoanRequestService {
+
+    private final LoanRequestRepository repository;
+
+    public LoanRequestServiceImpl(LoanRequestRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public LoanRequest apply(LoanRequest request) {
+        return repository.save(request);
+    }
+}
