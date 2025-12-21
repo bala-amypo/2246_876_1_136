d9da007a33b3923b00e7df7e21933c3a1d676f2f
bala-amypo <bala@amypo.in>
2025-12-21 07:06:02 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskAssessmentLogService.java
+++ src/main/java/com/example/demo/service/RiskAssessmentLogService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import com.example.demo.entity.RiskAssessmentLog;
 
-public interface RiskAssessmentService {
+public interface RiskAssessmentLogService {
 
     RiskAssessmentLog assessRisk(Long loanRequestId);
 
-    RiskAssessmentLog getByLoanRequestId(Long loanRequestId);
+    List<RiskAssessmentLog> getByLoanRequestId(Long loanRequestId);
 }
