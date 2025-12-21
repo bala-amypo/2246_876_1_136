9c7555c4c744003219ee8f197fcfb89656e7618f
bala-amypo <bala@amypo.in>
2025-12-21 09:15:37 +0000

    User commit

--- src/main/java/com/example/demo/service/EligibilityService.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.service;
-
-public interface EligibilityService {
-    Object checkEligibility(Long userId);
-}
--- src/main/java/com/example/demo/service/LoanRequestService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.LoanRequest;
-import java.util.List;
-
-public interface LoanRequestService {
-    LoanRequest createLoanRequest(LoanRequest request);
-    List<LoanRequest> getLoansByUserId(Long userId);
-}
