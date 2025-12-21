d6ae441ce4f97e08520bf7d7f67f14d1da00ef54
bala-amypo <bala@amypo.in>
2025-12-21 09:48:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import com.example.demo.security.JwtUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return jwtUtil;
+    }
+}
