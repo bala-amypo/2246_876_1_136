0f9d44e672bcce630a21ce1e15992c8e5684a6f5
bala-amypo <bala@amypo.in>
2025-12-25 18:11:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskAssessmentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskAssessmentServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.RiskAssessmentLog;
-import com.example.demo.repository.RiskAssessmentRepository;
+import com.example.demo.repository.RiskAssessmentLogRepository;
 import com.example.demo.service.RiskAssessmentService;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +10,9 @@ import java.util.List;
 @Service
 public class RiskAssessmentServiceImpl implements RiskAssessmentService {
 
-    private final RiskAssessmentRepository repository;
+    private final RiskAssessmentLogRepository repository;
 
-    public RiskAssessmentServiceImpl(RiskAssessmentRepository repository) {
+    public RiskAssessmentServiceImpl(RiskAssessmentLogRepository repository) {
         this.repository = repository;
     }
 
