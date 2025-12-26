8cca28fa0692d36ba9fba56499b8cff2ef41769b
bala-amypo <bala@amypo.in>
2025-12-26 04:56:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskAssessmentRepository.java
+++ src/main/java/com/example/demo/repository/RiskAssessmentRepository.java
@@ -4,7 +4,7 @@ import com.example.demo.entity.RiskAssessmentLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface RiskAssessmentLogRepository extends JpaRepository<RiskAssessmentLog, Long> {
+public interface RiskAssessmentRepository extends JpaRepository<RiskAssessmentLog, Long> {
 
     List<RiskAssessmentLog> findByLoanRequestId(Long loanRequestId);
 }
