6d9e4917cb0842d40ed2350b69c2d1c49ee18f89
bala-amypo <bala@amypo.in>
2025-12-26 14:32:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/LoanRequestServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.LoanRequest;
+import com.example.demo.repository.LoanRequestRepository;
+import com.example.demo.repository.UserRepository; // <--- ADD THIS IMPORT
+import com.example.demo.service.LoanRequestService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoanRequestServiceImpl implements LoanRequestService {
+    private final LoanRequestRepository repository;
+    private final UserRepository userRepository; 
+
+    public LoanRequestServiceImpl(LoanRequestRepository repository, UserRepository userRepository) {
+        this.repository = repository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public LoanRequest submitRequest(LoanRequest request) { 
+        return repository.save(request); 
+    }
+
+    @Override
+    public List<LoanRequest> getRequestsByUser(Long userId) { 
+        return repository.findByUserId(userId); 
+    }
+
+    @Override
+    public LoanRequest getById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Not found"));
+    }
+
+    @Override
+    public List<LoanRequest> getAllRequests() { 
+        return repository.findAll(); 
+    }
+}
