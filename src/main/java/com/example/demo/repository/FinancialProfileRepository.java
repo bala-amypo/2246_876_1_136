b24275e543c9d04325bc9dbc8837937d03b5fe49
bala-amypo <bala@amypo.in>
2025-12-26 18:03:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/FinancialProfileRepository.java
+++ src/main/java/com/example/demo/repository/FinancialProfileRepository.java
@@ -1,10 +1,20 @@
-package com.example.demo.repository;
+// package com.example.demo.repository;
 
-import com.example.demo.entity.FinancialProfile;
+// import com.example.demo.entity.FinancialProfile;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import java.util.Optional;
+
+// public interface FinancialProfileRepository extends JpaRepository<FinancialProfile, Long> {
+
+//     Optional<FinancialProfile> findByUserId(Long userId);
+// }
+package com.example.demo;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface FinancialProfileRepository extends JpaRepository<FinancialProfile, Long> {
-
     Optional<FinancialProfile> findByUserId(Long userId);
 }
