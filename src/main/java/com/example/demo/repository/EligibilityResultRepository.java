3b688510d9a49e180c5eef50e2158d4fd25d57c0
bala-amypo <bala@amypo.in>
2025-12-27 09:55:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/EligibilityResultRepository.java
+++ src/main/java/com/example/demo/repository/EligibilityResultRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.EligibilityResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.EligibilityResult;
 import java.util.Optional;
 
 public interface EligibilityResultRepository extends JpaRepository<EligibilityResult, Long> {
-    Optional<EligibilityResult> findByLoanRequestId(Long loanRequestId);
+    Optional<EligibilityResult> findByLoanRequestId(Long id);
 }
--- src/main/java/com/example/demo/repository/RiskAssessmentRepository.java
+++ src/main/java/com/example/demo/repository/RiskAssessmentRepository.java
@@ -1,12 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.RiskAssessment;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.example.demo.entity.RiskAssessment;
 import java.util.Optional;
 
-public interface RiskAssessmentRepository
-        extends JpaRepository<RiskAssessment, Long> {
-
-    Optional<RiskAssessment> findByLoanRequestId(Long loanRequestId);
+public interface RiskAssessmentRepository extends JpaRepository<RiskAssessment, Long> {
+    Optional<RiskAssessment> findByLoanRequestId(Long id);
 }
