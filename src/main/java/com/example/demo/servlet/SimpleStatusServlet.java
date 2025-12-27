3b8cb986073ccc02c93c44067effdf45744f8749
bala-amypo <bala@amypo.in>
2025-12-27 10:40:36 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -4,13 +4,13 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.*;
 import java.io.IOException;
 
-public class HealthCheckServlet extends HttpServlet {
+public class SimpleStatusServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
         resp.setContentType("text/plain");
-        resp.getWriter().write("Application is running");
+        resp.getWriter().write("OK");
     }
 }
